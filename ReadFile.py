e458b03cb034211cd197c52827c6697e74019dc9
Alex K <akikvadze@gmail.com>
2020-12-14 23:12:09 +0400

    Work version

--- ReadFile.py
+++ ReadFile.py
@@ -1,4 +1,6 @@
-name = input('enter file: ')
+#name = input('enter file: ')
+
+name = "F:\TMP\myFile.txt"
 handle = open(name, 'r')
 text = handle.read()
 words = text.split()
@@ -7,4 +9,16 @@ counts = dict()
 for word in words:
     counts[word] = counts.get(word, 0) + 1
 
-print(counts.values())
+# print(counts.__str__())
+
+
+bigcount = None
+bigword = None
+for words in counts.items():
+   # print(words)
+    count = words[1]
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+
+print(str(bigword) + " - " + str(bigcount))
